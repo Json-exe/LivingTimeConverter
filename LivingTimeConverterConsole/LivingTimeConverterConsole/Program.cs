5ac39e1da8873f7e06640fc3107c91c7aeeae650
Json-exe <lucaheitfeld@gmail.com>
2022-08-18 23:37:38 +0200

    Init - Console Client

---
+++ LivingTimeConverterConsole/LivingTimeConverterConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LivingTimeConverterConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
